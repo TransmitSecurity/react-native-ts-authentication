0b44eff346c5e142c4f0e2f56ccaf77aa4d5d88b
Shachar Udi <shachar@transmitsecurity.com>
2024-04-01 10:59:21 -0400

    MOB-862 #comment removed clientid and secret from config

--- example/src/config.ts
+++ example/src/config.ts
@@ -1,6 +1,6 @@
 export default {
-    clientId: "CLIENT_ID_FROM_PORTAL",
-    domain: "ASSOCIATED_DOMAIN",
-    secret: "SECRET_FROM_PORTAL", // Important! This is just for demo purposes. Never store your secret in the client side.
+    clientId: "REPLACE_WITH_CLIENT_ID",
+    domain: "REPLACE_WITH_DOMAIN",
+    secret: "REPLACE_WITH_SECRET", // Important! This is just for demo purposes. Never store your secret in the client side.
     baseUrl: "https://api.transmitsecurity.io", // eu = "api.eu.transmitsecurity.io" ca = "api.ca.transmitsecurity.io"
 }
